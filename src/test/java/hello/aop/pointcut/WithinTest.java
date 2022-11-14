d7c38c355e3bac04616e663f9732c54237af4951
jmr <jmr10200@gmail.com>
2022-11-14 22:42:11 +0900

    포인트컷 표현식 within 지시자 2

--- src/test/java/hello/aop/pointcut/WithinTest.java
+++ src/test/java/hello/aop/pointcut/WithinTest.java
@@ -3,6 +3,7 @@ package hello.aop.pointcut;
 import hello.aop.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 
@@ -35,6 +36,27 @@ public class WithinTest {
     @Test
     void withinSubPackage() {
         pointcut.setExpression("within(hello.aop..*)");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    /**
+     * 부모타입 지정시 : within
+     */
+    @Test
+    @DisplayName("타겟의 타입에만 직접 적용, 인터페이스를 선정하면 안됨")
+    void withinSuperTypeFalse() {
+        pointcut.setExpression("within(hello.aop.member.MemberService)");
+        // 주의 : 표현식에 부모타입을 지정하면 안된다. 정확하게 타입이 맞아야한다. (execution 과의 차이)
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isFalse();
+    }
+
+    /**
+     * 부모타입 지정시 : execution
+     */
+    @Test
+    @DisplayName("execution 은 타입 기반, 인터페이스를 선정 가능")
+    void executionSuperTypeTrue() {
+        pointcut.setExpression("execution(* hello.aop.member.MemberService.*(..))");
         assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
     }
 }
