4d19f09e864f6feb39a18aa1b5e0597c96328431
jmr <jmr10200@gmail.com>
2022-11-19 11:58:13 +0900

    AOP 예제 추가

---
+++ src/test/java/hello/aop/exam/ExamTest.java
@@ -0,0 +1,21 @@
+package hello.aop.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ExamTest {
+
+    @Autowired
+    ExamService examService;
+
+    @Test
+    void test() {
+        for (int i = 0; i < 5; i++) {
+            examService.request("data" + i);
+        }
+    }
+}
